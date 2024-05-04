31f0c6e6c2ad231b24127e1b021bc0a6654fba26
vsabouroux <129393338+vsabouroux@users.noreply.github.com>
2024-05-04 10:57:26 +0200

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import vslogo2 from "../../assets/vs-logo2.png";
+import cvPDF from "../../assets/Sabouroux_Valerie_CV_032024.pdf";
 import "./Header.scss";
 import { NavLink, useNavigate } from "react-router-dom";
-import * as PropTypes from 'prop-types';
+import * as PropTypes from "prop-types";
 // import styles from './Header.scss';
-import { useState } from 'react';
+import { useState } from "react";
 // import fontawesome from "@fortawesome/fontawesome-free";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // // import { library } from '@fortawesome/fontawesome-svg-core';
@@ -12,45 +13,77 @@ import { useState } from 'react';
 // import QRCode from "react-qr-code";
 
 function Header({ user, setUser }) {
-
   const navigate = useNavigate();
   const [isClicked, setIsClicked] = useState(false); // État local pour suivre si le lien a été cliqué
   const disconnect = () => {
     localStorage.clear();
     setUser(null);
-    navigate('/');
+    navigate("/");
   };
- // Réinitialise l'état isClicked lorsque l'ui clique sur un lien autre que "Se connecter"
- const handleLinkClick = () => {
-  setIsClicked(false);
-};
+  const handleQRCodeClick = () => {
+    // organisation du clic sur le qrcode pour ouvrir le cv stocké
+    window.open(cvPDF, "_blank");
+  };
+
+  // Réinitialise l'état isClicked lorsque l'ui clique sur un lien autre que "Se connecter"
+  const handleLinkClick = () => {
+    setIsClicked(false);
+  };
   return (
     <header className="vs-header">
       <div className="qr-code-container">
+        <div onClick={handleQRCodeClick} style={{ cursor: 'pointer' }}>
           {/* Ajout du QR code  avec le lien vers le CV */}
           {/* <QRCode value={qrCodeData} logo={vslogo} alt="logo de VS" className="qr-code" /> */}
-        <img src={vslogo2} alt="qr-code CV VS" className="qr-code-logo" />
+          <img src={vslogo2} alt="qr-code CV VS" className="qr-code-logo" />
+        </div>
       </div>
       <nav id="sidebar">
         <ul>
           <li className="accueil">
-            <NavLink to="/" className="nav-link"onClick={handleLinkClick}>
+            <NavLink to="/" className="nav-link" onClick={handleLinkClick}>
               Accueil
             </NavLink>
           </li>
           <li>
-            <NavLink to="/APropos" className="nav-link" onClick={handleLinkClick}>
+            <NavLink
+              to="/APropos"
+              className="nav-link"
+              onClick={handleLinkClick}
+            >
               A propos
             </NavLink>
           </li>
           <li className="contact">
-            <NavLink to="/Contact" className="nav-link"onClick={handleLinkClick}>
+            <NavLink
+              to="/Contact"
+              className="nav-link"
+              onClick={handleLinkClick}
+            >
               Contact
             </NavLink>
           </li>
           <li className="seconnecter">
-          {!user ? <NavLink to="/Connexion"className={isClicked ? "nav-link active" : "nav-link"} onClick={() => setIsClicked(true)}>Se connecter</NavLink> : <span tabIndex={0} role="button" onKeyUp={disconnect} onClick={disconnect}>Se déconnecter</span> } </li>
-          
+            {!user ? (
+              <NavLink
+                to="/Connexion"
+                className={isClicked ? "nav-link active" : "nav-link"}
+                onClick={() => setIsClicked(true)}
+              >
+                Se connecter
+              </NavLink>
+            ) : (
+              <span
+                tabIndex={0}
+                role="button"
+                onKeyUp={disconnect}
+                onClick={disconnect}
+              >
+                Se déconnecter
+              </span>
+            )}{" "}
+          </li>
+
           {/* <NavLink to="/SignIn" className="nav-link-signin">
               <FontAwesomeIcon
                 icon={faUser}
