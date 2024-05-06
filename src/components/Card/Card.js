37db26efe51d39a1ae7c6e7462e5532c813d8e71
vsabouroux <129393338+vsabouroux@users.noreply.github.com>
2024-05-06 14:54:10 +0200

    Add files via upload

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import "./Card.scss";
 // "Card" (vignettes de la pge Home) est le composant qui décrit de quoi il est fait
 //et la boucle "map" est faite sur la page qui doit afficher TOUS les logements soit la Home
-function Card({ title, imageUrl, id }) {
+
+function Card({ title, imageUrl, id, alt }) {
   return (
     <Link className="card-link" to={`/FicheProjet/${id}`}>
       <div className="Card">
         <div className="Gradient"></div> 
-        <img src={imageUrl} alt="projet" className="CardImage" />
+        <img src={imageUrl} alt={alt} className="CardImage" />
         <h3>{title}</h3>
-       
       </div>
     </Link>
   );
